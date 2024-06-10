f49a36ecf3ec6e07a5f93e265d806357f62a7307
calvincasalino <calvincasalino@gmail.com>
2024-06-10 18:33:52 -0400

    Initial commit

---
+++ src/targetCard.js
@@ -0,0 +1,15 @@
+import image1 from './assets/snapcaster-mage-1.jpg';
+import image2 from './assets/snapcaster-mage-2.jpg';
+import image3 from './assets/snapcaster-mage-3.jpg';
+import image4 from './assets/snapcaster-mage-4.jpg';
+import image5 from './assets/snapcaster-mage-5.jpg';
+
+export const targetCard = {
+
+    name: 'Snapcaster Mage',
+    image1: image1,
+    image2: image2,
+    image3: image3,
+    image4: image4,
+    image5: image5,
+  };
