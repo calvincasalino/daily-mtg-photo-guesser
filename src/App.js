c7993436987b1a34fc50a72f019661eb3b92cab6
Calvin Casalino <calvincasalino@users.noreply.github.com>
2024-06-13 12:38:57 -0400

    card and image refactor

--- src/App.js
+++ src/App.js
@@ -1,40 +1,50 @@
 import './App.css';
 import React, { useState } from 'react';
 import { Autocomplete, TextField, Button } from '@mui/material';
-import { cardList } from './cardList'
+import { cardList } from './cardList';
 import { targetCard } from './targetCard';
+import cover1 from './assets/Card Covers/imageCover1.png';
+import cover2 from './assets/Card Covers/imageCover2.png';
+import cover3 from './assets/Card Covers/imageCover3.png';
+import cover4 from './assets/Card Covers/imageCover4.png';
+import cover5 from './assets/Card Covers/imageCover5.png';
+import cover6 from './assets/Card Covers/imageCover6.png';
 
 function App() {
   const [inputValue, setInputValue] = useState('');
   const [guessNumber, setGuessNumber] = useState(1);
-  const [currentImage, setCurrentImage] = useState(targetCard.image1);
   const [boxColors, setBoxColors] = useState(Array(5).fill('white'));
   const [isCorrect, setIsCorrect] = useState(false);
   const [maxGuessesReached, setMaxGuessesReached] = useState(false);
+  const [currentCover, setCurrentCover] = useState(cover1);
+  const [previousCover, setPreviousCover] = useState(cover1);
+  const [showInitialCover, setShowInitialCover] = useState(true);
+
+  const coverImages = [cover1, cover2, cover3, cover4, cover5, cover6];
 
   const handleButtonClick = () => {
     const newBoxColors = [...boxColors];
     if (inputValue === targetCard.name) {
       newBoxColors[guessNumber - 1] = 'green';
       setIsCorrect(true);
-      setCurrentImage(targetCard.image6);
       setBoxColors(newBoxColors);
-      //alert('Correct! You guessed the right card.');
+      setPreviousCover(currentCover);
+      setCurrentCover(cover6);
     } else {
       newBoxColors[guessNumber - 1] = 'red';
       setBoxColors(newBoxColors);
-      //alert('Incorrect. Try again!');
       if (guessNumber === 5) {
         setMaxGuessesReached(true);
-        setCurrentImage(targetCard.image6);
+        setPreviousCover(currentCover);
+        setCurrentCover(cover6);
       } else {
-        setGuessNumber(prevGuessNumber => {
-          const newGuessNumber = prevGuessNumber + 1;
-          setCurrentImage(targetCard[`image${newGuessNumber}`]);
-          return newGuessNumber;
-        });
+        setPreviousCover(currentCover);
+        setCurrentCover(coverImages[guessNumber]);
+        setGuessNumber(prevGuessNumber => prevGuessNumber + 1);
       }
     }
+    setShowInitialCover(true);
+    setTimeout(() => setShowInitialCover(false), 2000);
   };
 
   const renderGrid = () => {
@@ -43,52 +53,62 @@ function App() {
     ));
   };
 
+  const ImageLayer = () => {
+    return (
+      <div className="image-container">
+        {showInitialCover && <img src={previousCover} alt="Cover" className="initial-cover-image" />}
+        <img src={currentCover} alt="Cover" className="cover-image" />
+        <img src={targetCard.image} alt="Card" className="card-image" />
+      </div>
+    );
+  };
+
   return (
     <div className="App">
       <h1>Daily MTG Photo Guesser</h1>
  
-      <img src={currentImage} alt='Card' style={{ width: 'auto', height: '600px' }} />
-      
+      <ImageLayer />
+    
       <div className="grid-container">
         {renderGrid()}
       </div>
 
-      {!isCorrect && !maxGuessesReached && (<div className="autocomplete-container">
-        <Autocomplete
-          disablePortal
-          id="combo-box"
-          options={cardList}
-          getOptionLabel={(option) => option.label}
-          sx={{ width: 400 }}
-          renderInput={(params) => <TextField {...params} label="Guess a card name" />}
-          noOptionsText=""
+      {!isCorrect && !maxGuessesReached && (
+        <div className="autocomplete-container">
+          <Autocomplete
+            disablePortal
+            id="combo-box"
+            options={cardList}
+            getOptionLabel={(option) => option.label}
+            sx={{ width: 400 }}
+            renderInput={(params) => <TextField {...params} label="Guess a card name" />}
+            noOptionsText=""
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            filterOptions={(options, { inputValue }) => {
+              if (inputValue === '') {
+                return [];
+              }
+              return options.filter(option =>
+                option.label.toLowerCase().includes(inputValue.toLowerCase())
+              );
+            }}
+          />
+        </div>
+      )}
 
-          inputValue={inputValue}
-          onInputChange={(event, newInputValue) => {
-            setInputValue(newInputValue);
-          }}
-          filterOptions={(options, { inputValue }) => {
-            if (inputValue === '') {
-              return [];
-            }
-            return options.filter(option =>
-              option.label.toLowerCase().includes(inputValue.toLowerCase())
-            );
-          }}
-        />
-      </div>)}
-
-      {!isCorrect && !maxGuessesReached && (<div className="button-container">
-        <Button variant="contained" color="primary" style={{ marginTop: '20px' }} onClick={handleButtonClick}>
-          Submit
-        </Button>
-      </div>)}
+      {!isCorrect && !maxGuessesReached && (
+        <div className="button-container">
+          <Button variant="contained" color="primary" style={{ marginTop: '20px' }} onClick={handleButtonClick}>
+            Submit
+          </Button>
+        </div>
+      )}
 
     </div>
   );
 }
 
-
 export default App;
-
-
--- src/targetCard.js
+++ src/targetCard.js
@@ -1,33 +1,14 @@
 import cards from './targetCardList.json';
 
-function importAll(r) {
-  let images = {};
-  r.keys().forEach((item) => {
-    images[item.replace('./', '')] = r(item);
-  });
-  return images;
-}
-
-const images = importAll(require.context('./assets', true, /\.(png|jpe?g|svg)$/));
-
 const getCurrentCard = () => {
-  const today = new Date().toLocaleDateString("en-CA", { timeZone: "America/New_York" }); // Get today's date in YYYY-MM-DD format
+  const today = new Date().toLocaleDateString("en-CA", { timeZone: "America/New_York" });
   const currentCard = cards.find(card => card.date === today);
   return currentCard;
 };
 
 const currentCard = getCurrentCard();
 
-const getImagePath = (path) => {
-  return images[path.replace('./', '')];
-};
-
 export const targetCard = {
   name: currentCard.name,
-  image1: getImagePath(currentCard.images[0]),
-  image2: getImagePath(currentCard.images[1]),
-  image3: getImagePath(currentCard.images[2]),
-  image4: getImagePath(currentCard.images[3]),
-  image5: getImagePath(currentCard.images[4]),
-  image6: getImagePath(currentCard.images[5]),
+  image: currentCard.image
 };
